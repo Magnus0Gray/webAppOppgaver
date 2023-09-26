1474020d67fa8cf5f9eba1e43e03e76fcd1bea24
Magnus0Gray <111729773+Magnus0Gray@users.noreply.github.com>
2023-09-26 09:33:25 +0200

    Jobbet vider

--- live-challenges/19.09.23-next-intro/src/oppgaver/oppgave1.tsx
+++ live-challenges/19.09.23-next-intro/src/oppgaver/oppgave1.tsx
@@ -4,38 +4,55 @@ const firstName = "Marius" //string
 const age = 38 //number
 const gender = genders[0] //array index 0, = 'male'
 const hasKids = true //boolean
-const data = { name: firstName, age, gender } //object
+const data = { name: firstName, age, gender } //object\
 
 
-//array med objekter
-const items = [{
+
+//b
+
+//svar:
+type Item = {
+	id: number
+	name: string
+}
+
+/*const items = [{
 	id: 1,
 	name: "Sara"
-}] 
+}]*/
+//array med objekter
 
+const items: Item[] = [{
+	id: 1,
+	name: "Sara"
+}]
 
 function run(doc) {
 	return doc.length > 0
-}
+} //returnerer number 1
 
 run(items)
 
+
+
 //c
 const validateAge = (age: number) => {
-	if (age < 18) {
-		return { allowed: false, data: 'Use bike' }
+	if (age >= 18) {
+		return { allowed: false, data: 'Use bike' } //returned false if under 18, kan enten sjekke for over 18, eller return true i stedet
+	}
+	if (age === 18) {
+		return { allowed: true, data: 'Drive your dads card' } //needs to be before checking under 100
 	}
 	if (age < 100) {
 		return { allowed: true, data: 'Drive your own car' }
 	}
-	if (age === 18) {
-		return { allowed: true, data: 'Drive your dads card' }
-	}
+	return { allowed: false, data: "unknown" } //did not handle missed cases, eg over 100
 }
 
 describe("Validate Age", () => {
 	it("should allow bike if age under 18", () => {
 		const result = validateAge(17)
+		//trenger expects
 		// Use toStrictEqual to validate object
 	})
 	it("should allow to use own car if age between 19 and 100", () => {
